e85f7c4a3dd5a3dee279eace57fa1ed4a8ab7222
tburton12 <p.wrzesien96@gmail.com>
2020-08-09 13:10:57 +0200

    using select_related

--- books/views.py
+++ books/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
 from books.models import Book
-from authors.models import Author
 
 
 # Create your views here.
 def home(request):
-    books_objects = Book.objects.all()
-    authors_objects = Author.objects.all()
+    books = Book.objects.select_related('author').all()
 
-    prepared_books = []
-    for book in books_objects:
-        author_dict = next(author for author in authors_objects if author.id == book.author_id)
-        current_book = {
-            "title": book.title,
-            "author": author_dict.name + " " + author_dict.surname
-        }
-        prepared_books.append(current_book)
-
-    return render(request, 'books/home.html', {'books': prepared_books})
+    return render(request, 'books/home.html', {'books': books})
 
